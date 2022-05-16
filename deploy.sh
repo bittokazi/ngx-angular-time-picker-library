bc3cd0a8071efbaf1cac44b0ed4968b8ec4ca3d4
bittokazi <bitto.kazi@gmail.com>
2022-05-16 17:41:17 +0200

    :sparkles: added descriptions

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 npm install
 npm run build:demo
-sudo docker-compose down || true
 docker build -t ngx-angular-time-picker-lib .
+sudo docker-compose down || true
 sleep 2s
 sudo docker-compose up -d
